d4412a02db62f0c50277e01003ed316342ba972c
pstupka <patryk.stupka@gmail.com>
2021-06-15 22:31:41 +0200

    minor tweaks with enemy ships

--- src/levels/TestLevel.gd
+++ src/levels/TestLevel.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
+export (PackedScene) var enemy_scene = preload("res://src/actors/EnemyShip.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _on_EnemySpawnTimer_timeout():
+	var enemy = enemy_scene.instance()
+	get_tree().current_scene.add_child(enemy)
 
+	enemy.global_position = $Enemies.global_position
